0e1e33a7dca3ea46ca6594eddbdf7e49b702a018
pavel_sol <pavel.solov@inbox.ru>
2020-12-15 17:32:10 +0400

    добавил возможность сохранения данных в  localstorage

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 let form = document.querySelector('.todo-control')
 let todoList = document.querySelector('#todo')
-let comletedList = document.querySelector('#comleted')
+let completedList = document.querySelector('#completed')
 let formInput = document.querySelector('.form__input')
 let data = {
     todo: [],
     completed: [],
 }
+
+if (localStorage.getItem('LocalData')) {
+    data = JSON.parse(localStorage.getItem('LocalData'))
+}
+
 
 form.addEventListener('submit', function (event) {
     event.preventDefault()
@@ -18,14 +23,26 @@ function addItem(text) {
     data.todo.push(text)
     renderItem(text)
     formInput.value = '';
+
+    dataUpdateToLocalS()
 }
 
-function renderItem(text) {
+
+
+
+function renderItem(text, completed = false) {
     const item = document.createElement('li')
     const itemDest = document.createElement('div')
     const todoButtons = document.createElement('div')
     const todoCompleteBtn = document.createElement('button')
     const todoRemoveBtn = document.createElement('button')
+
+    let list
+    if (completed) {
+        list = completedList
+    } else {
+        list = todoList
+    }
 
     todoRemoveBtn.addEventListener('click', function (event) {
         removeItem(event.target)
@@ -41,8 +58,7 @@ function renderItem(text) {
     todoCompleteBtn.classList.add('todo__complete')
     todoRemoveBtn.classList.add('todo__remove')
 
-
-    todoList.insertBefore(item, todoList.childNodes[0])
+    list.insertBefore(item, list.childNodes[0])
     item.appendChild(itemDest)
     itemDest.textContent = text
     item.appendChild(todoButtons)
@@ -50,14 +66,26 @@ function renderItem(text) {
     todoButtons.appendChild(todoRemoveBtn)
 }
 
+
+
 function removeItem(elem) {
     let item = elem.closest('.todo__item')
     let itemParent = elem.closest('.todo__item').closest('.todo')
     let id = itemParent.id
     let text = item.querySelector('.todo__desc').textContent
 
+    if (id == 'todo') {
+        data.todo.splice(data.todo.indexOf(text), 1)
+    } else {
+        data.completed.splice(data.completed.indexOf(text), 1)
+    }
+
     itemParent.removeChild(item)
+
+    dataUpdateToLocalS()
 }
+
+
 
 
 function completeItem(elem) {
@@ -69,20 +97,48 @@ function completeItem(elem) {
     let target
 
     if (id == 'todo') {
-        target = comletedList
+        target = completedList
     } else {
         target = todoList
     }
 
     if (id == 'todo') {
-        data.todo.splice(data.todo.indexOf[text], 1)
+        data.todo.splice(data.todo.indexOf(text), 1)
         data.completed.push(text)
     } else {
-        data.completed.splice(data.todo.indexOf[text], 1)
+        data.completed.splice(data.todo.indexOf(text), 1)
         data.todo.push(text)
     }
 
 
     itemParent.removeChild(item)
     target.insertBefore(item, target.childNodes[0])
+
+    dataUpdateToLocalS()
 }
+
+// ---------------------------------------------------------
+// LocalStorage
+
+function dataUpdateToLocalS() {
+    localStorage.setItem('LocalData', JSON.stringify(data))
+}
+
+
+
+function renderItemsForUpdate() {
+    if (!data.todo.length && !data.completed.length) {
+        return
+    } else {
+        for (let i = 0; i < data.todo.length; i++) {
+            renderItem(data.todo[i])
+        }
+
+        for (let i = 0; i < data.completed.length; i++) {
+            renderItem(data.completed[i], true)
+        }
+    }
+}
+
+
+renderItemsForUpdate()
